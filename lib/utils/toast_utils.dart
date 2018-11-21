fa38433223fb28b68ceaeac03fc7bc052b7b6a62
ruiyu <zhangruiyu@hexindai.com>
2018-11-21 15:46:04 +0800

    toast更新

--- lib/utils/toast_utils.dart
+++ lib/utils/toast_utils.dart
@@ -1,14 +1,7 @@
-//import 'package:fluttertoast/fluttertoast.dart';
 import 'package:oktoast/oktoast.dart';
+
 class ToastUtils {
   static toast(String msg) {
     showToast("msg");
-    /*Fluttertoast.showToast(
-        msg: msg,
-        toastLength: Toast.LENGTH_SHORT,
-        gravity: ToastGravity.CENTER,
-        timeInSecForIos: 1,
-        bgcolor: "#e74c3c",
-        textcolor: '#ffffff');*/
   }
 }
